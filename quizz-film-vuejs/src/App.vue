3c668c92eba1f89381e50eb5bdc62895f6cad2be
Antoine Quintin <antoine-quintin@le-campus-numerique.fr>
2025-06-20 16:28:35 +0200

    Base files

---
+++ quizz-film-vuejs/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <ProgressBar />
+  </div>
+<div>
+  <Button />
+</div>
+
+</template>
+
+<script setup>
+import ProgressBar from './components/ProgressBar.vue'
+import Button from './components/Button.vue'
+import { ref,onMounted,onUnmounted } from 'vue'
+import quizz from './quizz'
+</script>
+
+<style>
+
+</style>
---
+++ quizz-film-vuejs/src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+
+<button>
+  <input type="checkbox">{{}}
+</button>
+
+</template>
+
+<script>
+import { ref,onMounted,onUnmounted } from 'vue'
+import from './composable/use.js'
+import rawQuizzData from '../quizz-data.json'
+
+function showQuizz(title: string) {
+  const quizz = rawtechData.find(t => t.title.toLowerCase() === title.toLowerCase());
+  showQuizz().value = techno ?? null;
+}
+
+onMounted(() => {
+  showTechno('Front-end');
+});
+</script>
+
+<style>
+
+
+</style>
